b2a247a7f16166c27057d3d74cc3344207168272
Chris Franklin <cf73021@gmail.com>
2018-12-17 19:32:57 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -32,6 +32,6 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name, ingredient_ids:[])
+    params.require(:recipe).permit(:name, :ingredient_ids => [])
   end
 end
